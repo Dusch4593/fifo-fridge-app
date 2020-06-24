78e1627b807159afedba5b070625255b56713790
Dusch4593 <brandondusch@gmail.com>
2020-06-24 11:31:19 -0400

    added seed data for fridges and food_items

---
+++ fifo-fridge-app-backend/db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_06_24_115750) do
+
+  create_table "food_items", force: :cascade do |t|
+    t.string "name"
+    t.string "food_group"
+    t.string "expiration_date"
+    t.integer "quantity"
+    t.integer "fridge_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["fridge_id"], name: "index_food_items_on_fridge_id"
+  end
+
+  create_table "fridges", force: :cascade do |t|
+    t.string "name"
+    t.integer "capacity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "food_items", "fridges"
+end
--- fifo-fridge-app-backend/db/seeds.rb
+++ fifo-fridge-app-backend/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+fridge_a = Fridge.create(name: "Fridge A", capacity: 50)
+fridge_b = Fridge.create(name: "Fridge B", capacity: 13)
+fridge_c = Fridge.create(name: "Fridge C", capacity: 28)
+
+food_item_1a = fridge_a.food_items.create(name: "Banana", food_group: "fruit", expiration_date: "02/23/2020", quantity: 5)
+food_item_2a = fridge_a.food_items.create(name: "Bacon (packaged)", food_group: "meat", expiration_date: "7/01/2020", quantity: 1)
+food_item_3a = fridge_a.food_items.create(name: "Ketchup", food_group: "vegetable", expiration_date: "08/24/2020", quantity: 3)
+
+food_item_1b = fridge_b.food_items.create(name: "Cod", food_group: "seafood", expiration_date: "06/25/2020", quantity: 2)
+food_item_2b = fridge_b.food_items.create(name: "Lemons", food_group: "fruit", expiration_date: "08/01/2020", quantity: 5)
+food_item_3b = fridge_b.food_items.create(name: "Milk", food_group: "dairy", expiration_date: "06/30/2020", quantity: 1)
+
+food_item_1c = fridge_c.food_items.create(name: "Potatoes", food_group: "vegetable", expiration_date: "08/01/2020", quantity: 2)
+food_item_2c = fridge_c.food_items.create(name: "Milk", food_group: "dairy", expiration_date: "02/02/2020", quantity: 5)
+food_item_3c = fridge_c.food_items.create(name: "Avocado", food_group: "oils", expiration_date: "06/25/2020", quantity: 1)
