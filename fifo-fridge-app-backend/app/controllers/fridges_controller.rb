d860918db3b5507717f886c8c476556c1e157c3d
Dusch4593 <brandondusch@gmail.com>
2020-06-24 12:50:29 -0400

    added fridges#index and fridges#show actions to fridges_controller

--- fifo-fridge-app-backend/app/controllers/fridges_controller.rb
+++ fifo-fridge-app-backend/app/controllers/fridges_controller.rb
@@ -1,2 +1,11 @@
 class FridgesController < ApplicationController
+  def index
+    @fridges = Fridge.all
+    render json: FridgeSerializer.new(@fridges).to_serialized_json
+  end
+
+  def show
+    @fridge = Fridge.find_by(id: params[:id])
+    render json: FridgeSerializer.new(@fridge).to_serialized_json
+  end
 end
