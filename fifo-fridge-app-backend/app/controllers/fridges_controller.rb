3a182f3f85ce239ec12b7b4c08b6f7490357a100
Dusch4593 <brandondusch@gmail.com>
2020-06-28 07:29:32 -0400

    DRYed out the process of setting the fridge by :id

--- fifo-fridge-app-backend/app/controllers/fridges_controller.rb
+++ fifo-fridge-app-backend/app/controllers/fridges_controller.rb
@@ -1,11 +1,20 @@
 class FridgesController < ApplicationController
+  before_action: :set_fridge, only: [:show, :destroy]
   def index
     @fridges = Fridge.all
     render json: FridgeSerializer.new(@fridges).to_serialized_json
   end
 
   def show
-    @fridge = Fridge.find_by(id: params[:id])
     render json: FridgeSerializer.new(@fridge).to_serialized_json
+  end
+
+  def destroy
+    @fridge.destroy
+  end
+
+  private
+  def set_fridge
+    @fridge = Fridge.find_by(id: params[:id])
   end
 end
