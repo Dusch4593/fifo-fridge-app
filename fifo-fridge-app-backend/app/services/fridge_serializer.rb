fd5c0c4f4c91d8283474c793032aa51a80052caa
Dusch4593 <brandondusch@gmail.com>
2020-06-28 07:16:17 -0400

    added options hash containing both native data and related-model data

--- fifo-fridge-app-backend/app/services/fridge_serializer.rb
+++ fifo-fridge-app-backend/app/services/fridge_serializer.rb
@@ -5,6 +5,9 @@ class FridgeSerializer
 
   def to_serialized_json
     options = {
+      include: [food_items: {
+        except: [:updated_at]
+        }],
       except: [:updated_at]
     }
     @fridge.to_json(options)
