e2802dd663e579440c8f2a0907e753b5ba9cc2bc
Dusch4593 <brandondusch@gmail.com>
2020-06-28 21:49:03 -0400

    added strong params for new fridge and food_item attribute data

--- fifo-fridge-app-backend/app/controllers/fridges_controller.rb
+++ fifo-fridge-app-backend/app/controllers/fridges_controller.rb
@@ -1,5 +1,5 @@
 class FridgesController < ApplicationController
-  before_action: :set_fridge, only: [:show, :destroy]
+  before_action :set_fridge, only: [:show, :destroy]
   def index
     @fridges = Fridge.all
     render json: FridgeSerializer.new(@fridges).to_serialized_json
@@ -16,5 +16,9 @@ class FridgesController < ApplicationController
   private
   def set_fridge
     @fridge = Fridge.find_by(id: params[:id])
+  end
+
+  def fridge_params
+    params.require(:fridge).permit(:id, :name, :capacity, food_items_attributes: [:id, :name, :food_group, :expiration_date, :quantity, :created_at])
   end
 end
