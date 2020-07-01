fa124f4e626e4ed820b61852c9308d106b3b06c8
Dusch4593 <brandondusch@gmail.com>
2020-07-01 14:42:55 -0400

    tried to include if-statement that labels a FoodItem EXPIRED if today's date is past the exp. date (WIP)

--- fifo-fridge-app-frontend/models/food_item.js
+++ fifo-fridge-app-frontend/models/food_item.js
@@ -10,6 +10,12 @@ class FoodItem {
     let foodItemsContainer = document.createElement("div");
     foodItemsContainer.setAttribute("class", "food-items-container");
 
+    let today = new Date()
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+
     items.sort((a, b) => parseInt(a.expiration_date.replace('/', '')) - parseInt(b.expiration_date.replace('/', '')))
 
     for(let item of items) {
@@ -17,17 +23,74 @@ class FoodItem {
       let newFoodItem = new FoodItem(name, food_group, expiration_date, quantity);
       let foodItemCard = document.createElement("div");
       foodItemCard.setAttribute("class", "food-item-card");
-      foodItemCard.innerHTML = `
+
+
+      if(parseInt(item.expiration_date.replace('/','') < parseInt(today.replace('/', '')))) {
+        foodItemCard.innerHTML += `
+          <strong>${item.name} (EXPIRED)</strong> <br />
+          <strong>Food Group: </strong> ${item.food_group[0].toUpperCase() + item.food_group.slice(1)} <br />
+          <strong>Expiration Date: </strong> ${item.expiration_date} <br />
+          <strong>Quantity: </strong> ${item.quantity} <br /><br />`
+      };
+
+
+
+      foodItemCard.innerHTML += `
         <strong>${item.name}</strong> <br />
         <strong>Food Group: </strong> ${item.food_group[0].toUpperCase() + item.food_group.slice(1)} <br />
         <strong>Expiration Date: </strong> ${item.expiration_date} <br />
         <strong>Quantity: </strong> ${item.quantity} <br /><br />
       `
+
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerText = "X";
+      deleteBtn.setAttribute("class", "food-item-delete-btn");
+
+      deleteBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        API.deleteFoodItem(parseInt(e.target.parentElement.id))
+      });
+
+      foodItemCard.appendChild(deleteBtn);
       foodItemsContainer.appendChild(foodItemCard);
     };
 
-
     return foodItemsContainer.innerHTML;
 
+  };
+
+
+  static addItemToFridge(fridge, item) {
+    const {name, food_group, expiration_date, quantity} = item;
+    const newFoodItem = new FoodItem(name, food_group, expiration_date, quantity);
+    let foodItemsContainer = fridge.querySelector("#food-items-container");
+    let foodItemCard = document.createElement('div');
+    foodItemCard.setAttribute("class", "food-item-card");
+
+    foodItemCard.innerHTML += `
+      <strong>${item.name}</strong> <br />
+      <strong>Food Group: </strong> ${item.food_group[0].toUpperCase() + item.food_group.slice(1)} <br />
+      <strong>Expiration Date: </strong> ${item.expiration_date} <br />
+      <strong>Quantity: </strong> ${item.quantity} <br /><br />
+    `
+
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    deleteBtn.setAttribute("class", "food-item-delete-btn");
+    deleteBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      API.deleteFoodItem(parseInt(e.target.parentElement.id))
+    });
+
+    let linkBreak = document.createElement("br");
+    foodItemCard.appendChild(deleteBtn);
+    foodItemsContainer.appendChild(foodItemCard);
+
+    debugger;
+
+    fridge.getElementsByTagName("form")[0].reset();
+    fridge.getElementsByTagName("form")[0].style.display="none";
   };
 };
