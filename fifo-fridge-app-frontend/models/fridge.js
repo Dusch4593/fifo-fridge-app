285d900007db5ca5c51a21100feaab5a9b4914e1
Dusch4593 <brandondusch@gmail.com>
2020-07-01 14:43:53 -0400

    refactored fridgeCardHTML() method

--- fifo-fridge-app-frontend/models/fridge.js
+++ fifo-fridge-app-frontend/models/fridge.js
@@ -1,5 +1,5 @@
 class Fridge {
-  constructor(name, capacity=0, foodItems, id=null) {
+  constructor(name, capacity=1, foodItems, id=null) {
     if(Object.keys(foodItems).length <= capacity) {
       this.id = id;
       this.name = name;
@@ -14,8 +14,10 @@ class Fridge {
   fridgeCardHTML() {
     return `
     <h2>${this.name}</h2>
-    <h4>Capacity: ${this.capacity}</h4>` +
-    FoodItem.foodItemsCardHTML(this.foodItems);
+    <h4>Capacity: ${this.capacity}</h4>
+    <div id="food-items-container">` +
+    FoodItem.foodItemsCardHTML(this.foodItems) +
+    `</div>`;
   };
 
 
@@ -26,6 +28,14 @@ class Fridge {
     fridgeCard.setAttribute("id", this.id);
     fridgeCard.innerHTML += this.fridgeCardHTML();
 
+    const addFoodItem = document.createElement('button');
+    addFoodItem.innerText = "Add Food Item"
+    addFoodItem.addEventListener("click", (e) => {
+      e.preventDefault();
+      API.addFoodItem(fridgeCard, this.id);
+    });
+    fridgeCard.appendChild(addFoodItem);
+
     const deleteBtn = document.createElement("button");
     deleteBtn.innerText = "Delete";
     deleteBtn.setAttribute("class", "fridge-delete-btn");
@@ -34,6 +44,24 @@ class Fridge {
       API.deleteFridge(parseInt(e.target.parentElement.id))
     });
     fridgeCard.appendChild(deleteBtn);
+
+    let newFoodItemForm = document.createElement('form');
+    newFoodItemForm.id = "new-food-item-form";
+    newFoodItemForm.innerHTML = `
+    <input type="text" name="name" placeholder="food name" />
+    <br />
+    <input type="text" name="food_group" placeholder="food group" />
+    <br />
+    <input type="text" name="expiration_date" placeholder="expiration date (##/##/####)" />
+    <br />
+    <input type="number" name="quantity" placeholder="quantity" min=0 />
+    <br />
+    <input type="submit" value="Add Item" />
+    `
+
+    newFoodItemForm.style.display="none";
+    fridgeCard.appendChild(newFoodItemForm);
+
 
     fridgeContainer.appendChild(fridgeCard);
   };
