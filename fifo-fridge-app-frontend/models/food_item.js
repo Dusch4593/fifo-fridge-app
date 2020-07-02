9983c8ae54e01e24f354a5ad7f1f97e77b6b5323
Dusch4593 <brandondusch@gmail.com>
2020-07-02 18:12:27 -0400

    created functions to DRY out foodItemsCardHTML; fixed issue with delete listener not activating on click

--- fifo-fridge-app-frontend/models/food_item.js
+++ fifo-fridge-app-frontend/models/food_item.js
@@ -6,6 +6,8 @@ class FoodItem {
     this.quantity = quantity;
   };
 
+
+  // returns today's date as a string in mm/dd/yyyy format
   static todaysDate() {
     let today = new Date()
     let dd = String(today.getDate()).padStart(2, '0');
@@ -15,38 +17,33 @@ class FoodItem {
     return today;
   }
 
-  static foodItemsCardHTML(items) {
-    let foodItemsContainer = document.createElement("div");
-    foodItemsContainer.setAttribute("class", "food-items-container");
+  // sorts a fridge's food items by expiration date
+  static sortByDate(items) {
+    return items.sort((a, b) => parseInt(a.expiration_date.replace('/', '')) - parseInt(b.expiration_date.replace('/', '')))
+  };
 
 
+  // returns markup for a single item of a foodItemsCard <div>
+  static foodItemsCardHTML(item) {
+    return `
+    <h4>${item.name}</h4>
+    <strong>Food Group: ${item.food_group} </strong><br />
+    <strong>Expiration Date: ${item.expiration_date} </strong><br />
+    <strong>Quantity: ${item.quantity} </strong><br />
+    `
+  };
 
-    items.sort((a, b) => parseInt(a.expiration_date.replace('/', '')) - parseInt(b.expiration_date.replace('/', '')))
-
-    for(let item of items) {
+  // given a foodItemsContainer and an array of item-object data,
+  // this render
+  static renderFoodItems(foodItemsContainer, items) {
+    for(let item of this.sortByDate(items)) {
       const {name, food_group, expiration_date, quantity} = item;
       let newFoodItem = new FoodItem(name, food_group, expiration_date, quantity);
       let foodItemCard = document.createElement("div");
       foodItemCard.setAttribute("class", "food-item-card");
+      foodItemCard.id = item.id;
 
-
-      if(parseInt(item.expiration_date.replace('/','') < parseInt(this.todaysDate().replace('/', '')))) {
-        foodItemCard.innerHTML += `
-          <strong>${item.name} (EXPIRED)</strong> <br />
-          <strong>Food Group: </strong> ${item.food_group[0].toUpperCase() + item.food_group.slice(1)} <br />
-          <strong>Expiration Date: </strong> ${item.expiration_date} <br />
-          <strong>Quantity: </strong> ${item.quantity} <br /><br />`
-      };
-
-
-
-      foodItemCard.innerHTML += `
-        <strong>${item.name}</strong> <br />
-        <strong>Food Group: </strong> ${item.food_group[0].toUpperCase() + item.food_group.slice(1)} <br />
-        <strong>Expiration Date: </strong> ${item.expiration_date} <br />
-        <strong>Quantity: </strong> ${item.quantity} <br /><br />
-      `
-
+      foodItemCard.innerHTML += this.foodItemsCardHTML(item);
 
       const deleteBtn = document.createElement("button");
       deleteBtn.innerText = "X";
@@ -54,15 +51,16 @@ class FoodItem {
 
       deleteBtn.addEventListener("click", (e) => {
         e.preventDefault();
+        debugger;
         API.deleteFoodItem(parseInt(e.target.parentElement.id))
       });
 
       foodItemCard.appendChild(deleteBtn);
       foodItemsContainer.appendChild(foodItemCard);
+
+
     };
-
-    return foodItemsContainer.innerHTML;
-
+    return foodItemsContainer
   };
 
 
@@ -86,6 +84,7 @@ class FoodItem {
     deleteBtn.setAttribute("class", "food-item-delete-btn");
     deleteBtn.addEventListener("click", (e) => {
       e.preventDefault();
+      debugger;
       API.deleteFoodItem(parseInt(e.target.parentElement.id))
     });
 
