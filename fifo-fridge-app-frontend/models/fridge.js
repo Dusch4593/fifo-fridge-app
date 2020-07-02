55f42a80909a227d681cc5dcf923a9b2085d1f03
Dusch4593 <brandondusch@gmail.com>
2020-07-02 18:13:14 -0400

    refactored fridgeCardHTML() and renderFridge()

--- fifo-fridge-app-frontend/models/fridge.js
+++ fifo-fridge-app-frontend/models/fridge.js
@@ -11,16 +11,16 @@ class Fridge {
     };
   };
 
+  // returns markup for a fridgeCard <div>
   fridgeCardHTML() {
     return `
     <h2>${this.name}</h2>
     <h4>Capacity: ${this.capacity}</h4>
-    <div id="food-items-container">` +
-    FoodItem.foodItemsCardHTML(this.foodItems) +
-    `</div>`;
+    `
   };
 
 
+  // sets up the container elements for the fetched Fridge (and FoodItem) data
   renderFridge() {
     const fridgeContainer = document.getElementById("fridge-container");
     const fridgeCard = document.createElement("div");
@@ -28,6 +28,22 @@ class Fridge {
     fridgeCard.setAttribute("id", this.id);
     fridgeCard.innerHTML += this.fridgeCardHTML();
 
+    // create "food-items-container <div> for current fridgeCard"
+    // invoke FoodItem.renderFoodItems() to populate the container
+    const foodItemsContainer = document.createElement("div");
+    foodItemsContainer.setAttribute("class", "food-items-container");
+
+    // render the food items and then append to foodItemsContainer?
+    // TODO: Build FoodItem.renderFoodItems(foodItemsContainer, this.foodItems) <-- returns updated copy of foodItemsContainer with added food items
+    // renderedFoodItems = FoodItem.renderFoodItems(foodItemsContainer, this.foodItems)
+    // fridgeCard.appendChild(renderedFoodItems)
+
+    const renderedFoodItems = FoodItem.renderFoodItems(foodItemsContainer, this.foodItems);
+    fridgeCard.appendChild(renderedFoodItems);
+
+
+    // create "Add Food Item" button
+    // button comes with event listener that invokes API.addFoodItem() when clicked
     const addFoodItem = document.createElement('button');
     addFoodItem.innerText = "Add Food Item"
     addFoodItem.addEventListener("click", (e) => {
@@ -36,15 +52,7 @@ class Fridge {
     });
     fridgeCard.appendChild(addFoodItem);
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete";
-    deleteBtn.setAttribute("class", "fridge-delete-btn");
-    deleteBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      API.deleteFridge(parseInt(e.target.parentElement.id))
-    });
-    fridgeCard.appendChild(deleteBtn);
-
+    // form for adding a new food item to the current fridgeCard
     let newFoodItemForm = document.createElement('form');
     newFoodItemForm.id = "new-food-item-form";
     newFoodItemForm.innerHTML = `
@@ -59,10 +67,27 @@ class Fridge {
     <input type="submit" value="Add Item" />
     `
 
+    // form is attached to to current fridgeCard and set to "dislpay:none" by default
+    // (changes to "dislpay: block" when clicked and back to "none" when submitted)
     newFoodItemForm.style.display="none";
     fridgeCard.appendChild(newFoodItemForm);
 
 
+    // button for deleting the current fridgeCard when clicked
+    // uses API.deleteFridge() to send AJAX request to Rails API to
+    // delete fridge from database
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.setAttribute("class", "fridge-delete-btn");
+    deleteBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      API.deleteFridge(parseInt(e.target.parentElement.id))
+    });
+    fridgeCard.appendChild(deleteBtn);
+
+
+    // finally, we append the fridgeCard to the existing fridgeContainer
+    // it is now rendered
     fridgeContainer.appendChild(fridgeCard);
   };
 };
